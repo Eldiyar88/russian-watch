bd305b3ddd7dd7ff6680cf63a632d371730df3fc
fist333 <mr.achaban@gmail.com>
2024-01-18 17:08:26 +0300

    UI

--- src/components/pages/IndexPage.jsx
+++ src/components/pages/IndexPage.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import {
+  TextField, Button, Card, CardContent,
+} from '@mui/material';
 import axios from 'axios';
 
 export default function IndexPage() {
@@ -17,38 +21,52 @@ export default function IndexPage() {
   };
 
   return (
-    <div className="card mb-3 me-3" style={{ width: '18rem' }}>
-      <img
-        className="card-img-top"
-        alt="..."
-        src="https://www.100suvenirov.ru/data/Image/catalog/436367-chasy-naruchnye-muzhskie_b.jpg"
-      />
-      <form onSubmit={submitHandler}>
-        <input
-          name="name"
-          type="text"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="имя"
-        />
-        <input
-          name="email"
-          type="text"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="электронная почта"
-        />
-        <input
-          name="picture"
-          type="text"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="ссылка на экскиз"
-        />
-        <button className="btn btn-danger" type="submit">
-          Оставить заявку
-        </button>
-      </form>
-    </div>
+    <Card
+      className="card mb-3 me-3"
+      style={{
+        width: '18rem',
+        backgroundImage: 'url("https://img.freepik.com/free-photo/watercolor-texture-background-light-gray-wallpaper_53876-104729.jpg?w=1800&t=st=1705586268~exp=1705586868~hmac=96ab239dbee02ddfd91e1c537bc76d1015888f8da1349ef651c8fce2c54171f1")',
+        // backgroundSize: 'cover',
+        // backgroundPosition: 'center',
+        // backgroundOpacity: '80%',
+
+      }}
+    >
+      <CardContent>
+        <form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <TextField
+              fullWidth
+              label="Имя"
+              name="name"
+              variant="outlined"
+              placeholder="Введите ваше имя"
+            />
+          </div>
+          <div className="mb-3">
+            <TextField
+              fullWidth
+              label="Электронная почта"
+              name="email"
+              type="email"
+              variant="outlined"
+              placeholder="Введите вашу почту"
+            />
+          </div>
+          <div className="mb-3">
+            <TextField
+              fullWidth
+              label="Ссылка на эскиз"
+              name="picture"
+              variant="outlined"
+              placeholder="Вставьте ссылку на эскиз"
+            />
+          </div>
+          <Button variant="contained" color="secondary" type="submit">
+            Оставить заявку
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
