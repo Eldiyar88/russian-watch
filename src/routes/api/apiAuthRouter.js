24b380f0a03bee4f2212169f9dc31147e5d85b5f
Redk0rd <red.kord95@gmail.com>
2024-01-18 15:42:52 +0300

    final

--- src/routes/api/apiAuthRouter.js
+++ src/routes/api/apiAuthRouter.js
@@ -9,11 +9,14 @@ router.post('/login', async (req, res) => {
   if (!(inputUserName && inputPassword)) {
     return res.status(400).json({ message: 'All fields are required' });
   }
-  console.log(req.body);
+  console.log('1 ------> ', JSON.stringify(req.body));
+
   const user = await User.findOne({ where: { userName: inputUserName } });
+  console.log('2 ------> ', JSON.stringify(user));
+
   if (!user) return res.status(403).json({ message: 'No user found' });
 
-  if (user) return res.status(200).json({ message: 'ok' });
+  if (inputPassword === user.password) return res.status(200).json({ message: 'ok' });
 });
 
 export default router;
